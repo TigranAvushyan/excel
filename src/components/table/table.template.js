9b6f87d34e94ccb7d881519bff8982c327027a35
Tigran Avushyan <tigran.avushyan@gmail.com>
2020-10-31 14:15:46 +0400

    resize config

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,27 +1,28 @@
 const CODES = {
-  A: 65,
-  Z: 90,
+    A: 65,
+    Z: 90,
 }
 
-function toCell() {
-  return `
-    <div class="cell" contenteditable></div>
+function toCell(col) {
+    return `
+    <div class="cell" contenteditable data-letter="${col}"></div>
   `
 }
 
 function toColumn(col) {
-  return `
-    <div class="column">
+    return `
+    <div class="column" data-type="resizable" data-letter="${col}">
         ${col}
-        <div class="col-resize"></div>
+        <div class="col-resize" data-resize="col"></div>
     </div>
   `
 }
 
 function createRow(index, content) {
-  const resize = index ? `<div class="row-resize"></div>` : ''
-  return `
-    <div class="row">
+    const resize = index ? `<div class="row-resize" 
+        data-resize="row"></div>` : ''
+    return `
+    <div class="row" data-type="resizable">
       <div class="row-info">
         ${index ? index : ''}
         ${resize}
@@ -32,28 +33,29 @@ function createRow(index, content) {
 }
 
 function toChar(_, index) {
-  return String.fromCharCode(CODES.A + index)
+    return String.fromCharCode(CODES.A + index)
 }
 
 export function createTable(rowsCount = 15) {
-  const rows = []
-  const colsCount = CODES.Z - CODES.A + 1
-  const cols = new Array(colsCount)
-      .fill('')
-      .map(toChar)
-      .map(toColumn)
-      .join('')
-
-  rows.push(createRow(null, cols))
-
-  for (let i = 1; i <= rowsCount; i++) {
-    const cells = new Array(colsCount)
+    const rows = []
+    const colsCount = CODES.Z - CODES.A + 1
+    const cols = new Array(colsCount)
         .fill('')
-        .map(toCell)
+        .map(toChar)
+        .map(toColumn)
         .join('')
 
-    rows.push(createRow(i, cells))
-  }
+    rows.push(createRow(null, cols))
 
-  return rows.join('')
+    for (let i = 1; i <= rowsCount; i++) {
+        const cells = new Array(colsCount)
+            .fill('')
+            .map(toChar)
+            .map(toCell)
+            .join('')
+
+        rows.push(createRow(i, cells))
+    }
+
+    return rows.join('')
 }
